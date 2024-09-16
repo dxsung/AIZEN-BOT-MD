58e40632ca98ccf006e34d8056bc774a5e4b9314
dxsung <mohamed511827@gmail.com>
2024-09-16 04:28:31 +0300

    Update and rename random-Messi.js to صور-ميسي.js

--- plugins/صور-ميسي.js
+++ plugins/صور-ميسي.js
@@ -3,8 +3,8 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/Messi.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
 //conn.sendFile(m.chat, url, 'error.jpg', `*Messi 🇦🇷*`, m)}
-conn.sendButton(m.chat, "*Messi 🇦🇷*", wm, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `${usedPrefix + command}`]], null, null, m)}
-handler.help = ['messi']
-handler.tags = ['internet']
-handler.command = /^(messi)$/i
+conn.sendButton(m.chat, "> *انـكـرا مـيـسـي انـكـرا مـيـسـي*", wm, url, [['⬅️¦ الـمـزيـد ¦➡️', `${usedPrefix + command}`]], null, null, m)}
+handler.help = ['ميسي']
+handler.tags = ['صور']
+handler.command = /^(ميسي)$/i
 export default handler
