980ae0433bec0449767ae050647d342b5934dd01
dxsung <mohamed511827@gmail.com>
2024-09-16 04:25:39 +0300

    Update and rename random-C. Ronaldo.js to صور-رونالدو.js

--- plugins/صور-رونالدو.js
+++ plugins/صور-رونالدو.js
@@ -3,8 +3,8 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 let cristiano = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/CristianoRonaldo.json`)).data  
 let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
 //conn.sendFile(m.chat, ronaldo, 'error.jpg', `*Siiiuuuuuu*`, m)}
-conn.sendButton(m.chat, "*Siiiuuuuuu*", wm, ronaldo, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `${usedPrefix + command}`]], null, null, m)}
-handler.help = ['cristianoronaldo', 'cr7']
-handler.tags = ['internet']
-handler.command = /^(cristianoronaldo|cr7)$/i
+conn.sendButton(m.chat, "> *سووووووووووو*", wm, ronaldo, [['⬅️¦ الـمـزيـد ¦➡️', `${usedPrefix + command}`]], null, null, m)}
+handler.help = ['رونالدو', 'كرستيانو']
+handler.tags = ['صور']
+handler.command = /^(رونالدو|كرستيانو)$/i
 export default handler
